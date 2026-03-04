61e67106844790fbc863ea4c4df31a6c6ab94e02
Ralf van Meer <ralf.van.meer@ict.nl>
2026-03-04 13:51:22 +0100

    Add initial project files for HelloWorld application
    - Create .gitignore to exclude build directories
    - Add launch configuration for debugging in Visual Studio Code
    - Include HelloWorld.csproj project file
    - Implement basic Program.cs with a Hello World message
    - Create anchr_demo.sln solution file

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // Write naar het console: 'Het ingevoerde nummer is een primgetal' of 'Het ingevoerde nummer is geen priemgetal'
+        }
+    }
+}
